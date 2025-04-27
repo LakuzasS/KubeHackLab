d63bf3944152857b07746fc6f2d5a6d4309e2c83
Maxime BRODIN <mxb@MacBook-Pro-de-Maxime.local>
2025-04-27 22:15:21 +0200

    update deployment

--- SCRIPTS/deploy.sh
+++ SCRIPTS/deploy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 echo "💻 Déploiement du cluster Kubernetes avec Kind..."
-kind create cluster
+kind create cluster --config CLUSTER_SETUP/kind-config.yaml
 
 echo "🚀 Déploiement de Juice Shop..."
 kubectl apply -f DEPLOYMENTS/juice-shop-deployment.yaml
 
 echo "🔐 Configuration du Dashboard sécurisé..."
 kubectl apply -f DEPLOYMENTS/dashboard-admin-user.yaml
+
+echo "🔐 Récupération du token de connexion..."
+kubectl -n kubernetes-dashboard create token admin-user
 
 echo "✅ Déploiement terminé !"
